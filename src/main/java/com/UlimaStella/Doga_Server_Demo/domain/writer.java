49c758ccae1734c8010ee8229f44bc989bf9a244
Mustafa Gülsoy <mustafa30045@gmail.com>
2022-12-08 14:53:55 +0300

    0.1.0 first Commit

---
+++ src/main/java/com/UlimaStella/Doga_Server_Demo/domain/writer.java
@@ -0,0 +1,14 @@
+package com.UlimaStella.Doga_Server_Demo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class writer {
+}
