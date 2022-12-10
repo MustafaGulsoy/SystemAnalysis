560a03baa25a7a02604a4018a700e2ec5cd90a4f
Mustafa Gülsoy <mustafa30045@gmail.com>
2022-12-10 13:16:10 +0300

    0.1.2

--- src/main/java/com/UlimaStella/Doga_Server_Demo/DogaServerDemoApplication.java
+++ src/main/java/com/UlimaStella/Doga_Server_Demo/DogaServerDemoApplication.java
@@ -1,10 +1,16 @@
 package com.UlimaStella.Doga_Server_Demo;
 
+import com.UlimaStella.Doga_Server_Demo.domain.Role;
+import com.UlimaStella.Doga_Server_Demo.domain.User;
+import com.UlimaStella.Doga_Server_Demo.services.user.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class DogaServerDemoApplication {
