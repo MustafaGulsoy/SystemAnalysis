b99b141be073107831e7b04cd45bfdf947a72796
Mustafa Gülsoy <mustafa30045@gmail.com>
2022-12-10 07:07:50 +0300

    remove Database

--- src/main/java/com/UlimaStella/Doga_Server_Demo/DogaServerDemoApplication.java
+++ src/main/java/com/UlimaStella/Doga_Server_Demo/DogaServerDemoApplication.java
@@ -18,29 +18,29 @@ public class DogaServerDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DogaServerDemoApplication.class, args);
 	}
-
-	@Bean
-	CommandLineRunner run(UserService userService){
-		return args -> {
-			userService.saveRole(new Role(null,"ROLE_USER"));
-			userService.saveRole(new Role(null,"ROLE_ADMIN"));
-			userService.saveRole(new Role(null,"ROLE_MANAGER"));
-			userService.saveRole(new Role(null,"ROLE_SUPER_ADMIN"));
-
-			userService.saveUser(new User(null,"Mustafa Gulsoy1","Mustafa","1234",new ArrayList<>()));
-			userService.saveUser(new User(null,"Mustafa Gulsoy","user","1234",new ArrayList<>()));
-			userService.saveUser(new User(null,"Mustafa Gulsoy","manager","1234",new ArrayList<>()));
-			userService.saveUser(new User(null,"Mustafa Gulsoy","superAdmin","1234",new ArrayList<>()));
-
-			userService.addRoleToUser("Mustafa","ROLE_ADMIN");
-			userService.addRoleToUser("Mustafa","ROLE_USER");
-			userService.addRoleToUser("Mustafa","ROLE_SUPER_ADMIN");
-			userService.addRoleToUser("user","ROLE_USER");
-			userService.addRoleToUser("manager","ROLE_MANAGER");
-			userService.addRoleToUser("superAdmin","ROLE_SUPER_ADMIN" );
-
-		};
-	}
+//
+//	@Bean
+//	CommandLineRunner run(UserService userService){
+//		return args -> {
+//			userService.saveRole(new Role(null,"ROLE_USER"));
+//			userService.saveRole(new Role(null,"ROLE_ADMIN"));
+//			userService.saveRole(new Role(null,"ROLE_MANAGER"));
+//			userService.saveRole(new Role(null,"ROLE_SUPER_ADMIN"));
+//
+//			userService.saveUser(new User(null,"Mustafa Gulsoy1","Mustafa","1234",new ArrayList<>()));
+//			userService.saveUser(new User(null,"Mustafa Gulsoy","user","1234",new ArrayList<>()));
+//			userService.saveUser(new User(null,"Mustafa Gulsoy","manager","1234",new ArrayList<>()));
+//			userService.saveUser(new User(null,"Mustafa Gulsoy","superAdmin","1234",new ArrayList<>()));
+//
+//			userService.addRoleToUser("Mustafa","ROLE_ADMIN");
+//			userService.addRoleToUser("Mustafa","ROLE_USER");
+//			userService.addRoleToUser("Mustafa","ROLE_SUPER_ADMIN");
+//			userService.addRoleToUser("user","ROLE_USER");
+//			userService.addRoleToUser("manager","ROLE_MANAGER");
+//			userService.addRoleToUser("superAdmin","ROLE_SUPER_ADMIN" );
+//
+//		};
+//	}
 
 
 	@Bean
